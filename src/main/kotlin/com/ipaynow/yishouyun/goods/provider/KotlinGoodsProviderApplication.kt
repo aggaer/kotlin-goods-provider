f447687ce11c85458f76d6b1a801d3b4a3d90daa
jerry <yuanxing@chuangmj.com>
2018-11-01 09:22:36 +0800

    Initial commit

---
+++ src/main/kotlin/com/ipaynow/yishouyun/goods/provider/KotlinGoodsProviderApplication.kt
@@ -0,0 +1,11 @@
+package com.ipaynow.yishouyun.goods.provider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGoodsProviderApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinGoodsProviderApplication>(*args)
+}
